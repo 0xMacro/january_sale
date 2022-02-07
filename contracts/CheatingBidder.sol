d38a2263f5d822de4b639abb8d004b7f53da47fb
Alex Sumner <alex.sumner@gmail.com>
2022-02-07 14:43:31 +0000

    hardhat config, package.json and skeleton bidder contract

--- contracts/CheatingBidder.sol
+++ contracts/CheatingBidder.sol
@@ -1,24 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-import "hardhat/console.sol";
-
-abstract contract ISaleItem {
-    function reservePrice() external view virtual returns (uint);
-    function isSold() external view virtual returns (bool);
-    function buy() external virtual ;
-}
+pragma solidity ^0.8.10;
 
 contract CheatingBidder {
-    ISaleItem private saleItem;
 
     function cheat(address _saleItem) public {
-        saleItem = ISaleItem(_saleItem);
-        console.log("sale item ...");
-        saleItem.buy();
-        console.log("... sale item");
     }
 
-    function bidPrice() external view returns (uint256) {
-        return saleItem.isSold() ? 0 : 100;
-    }
 }
